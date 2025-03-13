564a0daffab8fd470b7b69fbe3043071c5b36543
Arnon Gabriel <arrnon.11@gmail.com>
2025-03-12 21:28:33 -0300

    link to about

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const appContext = useContext(AppContext)
         <div className="hero-text"> 
             <h1>{appContext.languages[appContext.language].hero.title}</h1>
             <p>{appContext.languages[appContext.language].hero.subtitle}</p>
-           <Link>
+           <Link to="/about">
             <Button buttonStyle="secondary" arrow> 
             {appContext.languages[appContext.language].hero.cta}
             </Button>
